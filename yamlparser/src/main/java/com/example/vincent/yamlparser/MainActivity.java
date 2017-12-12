44c2e57af1baf75f72ee3e390b2db7f50ceb960c
vincent <2315223212@qq.com>
2017-12-12 13:48:46 +0800

    Menu button without drag

--- yamlparser/src/main/java/com/example/vincent/yamlparser/MainActivity.java
+++ yamlparser/src/main/java/com/example/vincent/yamlparser/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.uni.utils.FrameProperty;
+import com.uni.utils.MenuButton.MenuButton;
 import com.uni.utils.Property;
 
 import java.util.ArrayList;
@@ -25,46 +27,73 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
 
-        YAMLParser parser = new YAMLParser(getFilesDir(), "test.yml");
-        parser.setBrief("vincent", "student", "12-11", "parser-url", "Test");
-        Property p1 = new Property(1);
-        Property p2 = new Property(2);
-        
-        parser.addFrame(1000, 1000);
-        parser.addElement(p1, "p1_url");
-        parser.addElement(p2, "p2_url");
-        
-        parser.addFrame(1000, 1000);
-        parser.addElement(new Property(3), "p3_url");
-        parser.addElement(new Property(4), "p4_url");
+        MenuButton menuButton = findViewById(R.id.menu_button);
+        menuButton.setOnItemClickListener(new MenuButton.OnItemClickListener() {
+            @Override
+            public void onNextButtonClick() {
+                Toast.makeText(MainActivity.this, "You've clicked  Next", Toast.LENGTH_SHORT).show();
+            }
 
-        //parser.addFrame(0.5f, 0.4f);
-        try{
-            parser.saveYAML();
-            Log.i(TAG, "onCreate: saved");
-            parser.loadYAML();
-            Log.i(TAG, "onCreate: loaded");
-
-            Log.i(TAG, "onCreate: "+parser.getMaxElementId() + parser.getFrameUrl() + parser.getTitle());
-
-            List<FrameProperty> frameProperties = new ArrayList<>();
-
-            while (parser.hasNextFrame() != -1){
-
-                frameProperties.add(parser.getFrame());
-                Log.i(TAG, "onCreate: " + (parser.getFrame()).Id);
-
-                while (parser.hasNextElement() != -1){
-
-                    Property property = parser.getElement();
-                    String url = parser.getElementUrl();
-                    Log.i(TAG, "onCreate: "+property.ID + " " + property.x + " " + property.y + " " + url
-                    );
-
+            @Override
+            public void onMenuItemClick(int position) {
+                switch (position){
+                    case 0:
+                        Toast.makeText(MainActivity.this, "You've clicked  0", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "You've clicked  1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "You've clicked  2", Toast.LENGTH_SHORT).show();
+                        break;
                 }
             }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
+            @Override
+            public void onPrevButtonClick() {
+                Toast.makeText(MainActivity.this, "You've clicked  Prev", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        YAMLParser parser = new YAMLParser(getFilesDir(), "test.yml");
+//        parser.setBrief("vincent", "student", "12-11", "parser-url", "Test");
+//        Property p1 = new Property(1);
+//        Property p2 = new Property(2);
+//
+//        parser.addFrame(1000, 1000);
+//        parser.addElement(p1, "p1_url");
+//        parser.addElement(p2, "p2_url");
+//
+//        parser.addFrame(1000, 1000);
+//        parser.addElement(new Property(3), "p3_url");
+//        parser.addElement(new Property(4), "p4_url");
+//
+//        //parser.addFrame(0.5f, 0.4f);
+//        try{
+//            parser.saveYAML();
+//            Log.i(TAG, "onCreate: saved");
+//            parser.loadYAML();
+//            Log.i(TAG, "onCreate: loaded");
+//
+//            Log.i(TAG, "onCreate: "+parser.getMaxElementId() + parser.getFrameUrl() + parser.getTitle());
+//
+//            List<FrameProperty> frameProperties = new ArrayList<>();
+//
+//            while (parser.hasNextFrame() != -1){
+//
+//                frameProperties.add(parser.getFrame());
+//                Log.i(TAG, "onCreate: " + (parser.getFrame()).Id);
+//
+//                while (parser.hasNextElement() != -1){
+//
+//                    Property property = parser.getElement();
+//                    String url = parser.getElementUrl();
+//                    Log.i(TAG, "onCreate: "+property.ID + " " + property.x + " " + property.y + " " + url
+//                    );
+//
+//                }
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
